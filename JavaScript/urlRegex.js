ddd16732ece9968da1d5936515894217fc32c28d
Jonny Deveson <jrdeveson@hotmail.com>
2022-12-14 17:31:58 +0000

    function name changes

--- JavaScript/urlRegex.js
+++ JavaScript/urlRegex.js
@@ -1,11 +1,11 @@
 const urlRegex = /\b(https?:\/\/|www\.)\S+/gm;
 
-const findUrls = (text) => {
+const returnArrayOfURLs = (text) => {
   const urls = text.match(urlRegex);
   return urls;
 };
 
-const replaceUrlsWithAnchors = (text) => {
+const wrapUrlsWithAnchorTags = (text) => {
   return text.replace(urlRegex, (url) => {
     return `<a href="${url}" target="_blank">${url}</a>`;
   });
